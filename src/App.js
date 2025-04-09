f036c27c9f719c877c63a90d816b48ae6e323780
Md-Mujahid92 <120643682+Md-Mujahid92@users.noreply.github.com>
2025-04-09 12:54:30 +0530

    add favicon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
-import logo from "./logo.svg";
 import "./App.css";
+import logo from "../public/logo512.png";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Welcome to Mujahid Resume</header>
+      <div className="App-body">
+        <div className="App-body-left">
+          <div className="App-body-left-top">
+            <div className="App-body-left-top-left">
+              <img src={logo} alt="logo" />
+            </div>
+            <div className="App-body-left-top-right">
+              <h1>Mujahid</h1>
+              <h2>Software Engineer</h2>
+            </div>
+          </div>
+          <div className="App-body-left-bottom">
+            <div className="App-body-left-bottom-left">
+              <h3>Skills</h3>
+              <ul>
+                <li>React</li>
+                <li>Node</li>
+                <li>Express</li>
+                <li>Python</li>
+                <li>Java</li>
+                <li>SQL</li>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>Git</li>
+                <li>GitHub</li>
+                <li>Linux</li>
+                <li>Windows</li>
+                <li>MacOS</li>
+                <li>VS Code</li>
+                <li>IntelliJ IDEA</li>
+                <li>Android Studio</li>
+                <li>Postman</li>
+                <li>PostgreSQL</li>
+                <li>MySQL</li>
+                <li>MongoDB</li>
+                <li>Redis</li>
+                <li>Docker</li>
+                <li>Kubernetes</li>
+                <li>Ansible</li>
+                <li>Terraform</li>
+                <li>OpenShift</li>
+                <li>OpenStack</li>
+                <li>OpenNebula</li>
+                <li>OpenVZ</li>
+                <li>OpenWRT</li>
+                <li>OpenBSD</li>
+                <li>OpenSolaris</li>
+                <li>OpenWRT</li>
+                <li>OpenWRT</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
